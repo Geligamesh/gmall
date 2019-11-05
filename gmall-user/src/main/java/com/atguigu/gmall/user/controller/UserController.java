1bda32e9ac357110201630d19910a337feda81e6
Geligamesh <535758155@qq.com>
2019-11-05 21:42:37 +0800

    聚合工程的搭建

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.atguigu.gmall.user.controller;
+
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.bean.UmsMember;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @RequestMapping("getReceiveAddressByMemberId")
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+        return userService.getReceiveAddressByMemberId(memberId);
+    }
+
+
+    @RequestMapping("getAllUser")
+    public List<UmsMember> getAllUser(){
+
+        List<UmsMember> umsMembers = userService.getAllUser();
+
+        return umsMembers;
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index(){
+        return "hello user";
+    }
+
+
+
+}
