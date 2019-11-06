5f42268221cee6eb5deda87ec515d333dd28eeb0
Geligamesh <535758155@qq.com>
2019-11-06 23:27:01 +0800

    聚合工程的搭建

--- gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user-web/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -4,43 +4,30 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.UmsMember;
 import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
 import com.atguigu.gmall.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class UserController {
 
     @Reference
-    UserService userService;
+    private UserService userService;
 
     @RequestMapping("getReceiveAddressByMemberId")
     public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
 
-        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
-
-        return umsMemberReceiveAddresses;
+        return userService.getReceiveAddressByMemberId(memberId);
     }
 
 
     @RequestMapping("getAllUser")
     @ResponseBody
     public List<UmsMember> getAllUser(){
-
-        List<UmsMember> umsMembers = userService.getAllUser();
-
-        return umsMembers;
+        return userService.getAllUser();
     }
-
-    @RequestMapping("index")
-    @ResponseBody
-    public String index(){
-        return "hello user";
-    }
-
 
 
 }
